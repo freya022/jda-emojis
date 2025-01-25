98bad3a4ace4298977e181ccd7848b37f070972c
freya02 <41875020+freya022@users.noreply.github.com>
2025-01-26 00:23:48 +0100

    Use Discord emoji naming (#1)

--- src/test/kotlin/dev/freya02/jda/emojis/EmojisTests.kt
+++ src/test/kotlin/dev/freya02/jda/emojis/EmojisTests.kt
@@ -1,27 +1,22 @@
 package dev.freya02.jda.emojis
 
 import net.dv8tion.jda.api.entities.emoji.UnicodeEmoji
-import net.fellbaum.jemoji.Emoji
-import net.fellbaum.jemoji.Emojis
+import net.fellbaum.jemoji.EmojiManager
 import org.junit.jupiter.api.Test
 import kotlin.reflect.KProperty0
 import kotlin.reflect.full.staticProperties
 import kotlin.reflect.jvm.isAccessible
-import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 object EmojisTests {
 
     @Test
     fun `Match unicode of JDA and JEmoji`() {
-        val jemojiProperties = Emojis::class.staticProperties
-
         UnicodeEmojis::class.staticProperties
             .filter { it.returnType.classifier == UnicodeEmoji::class }
             .forEach { jdaProperty ->
                 val unicodeEmoji = jdaProperty.getAccessible() as UnicodeEmoji
-                val emoji = jemojiProperties.first { it.name == jdaProperty.name }.getAccessible() as Emoji
-
-                assertEquals(emoji.emoji, unicodeEmoji.name)
+                assertTrue(EmojiManager.isEmoji(unicodeEmoji.name))
             }
     }
 
