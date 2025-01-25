3f7c978a3cc764344669b6aabb27bb4968c30e15
freya02 <41875020+freya022@users.noreply.github.com>
2025-01-26 00:18:45 +0100

    Rework tests

--- src/test/kotlin/dev/freya02/jda/emojis/EmojisTests.kt
+++ src/test/kotlin/dev/freya02/jda/emojis/EmojisTests.kt
@@ -1,24 +1,23 @@
 package dev.freya02.jda.emojis
 
+import net.dv8tion.jda.api.entities.emoji.UnicodeEmoji
+import net.fellbaum.jemoji.EmojiManager
 import org.junit.jupiter.api.Test
 import kotlin.reflect.KProperty0
+import kotlin.reflect.full.staticProperties
 import kotlin.reflect.jvm.isAccessible
+import kotlin.test.assertTrue
 
 object EmojisTests {
 
     @Test
     fun `Match unicode of JDA and JEmoji`() {
-        //TODO don't match with name as they differ, just see if the unicode exists
-//        val jemojiProperties = Emojis::class.staticProperties
-//
-//        UnicodeEmojis::class.staticProperties
-//            .filter { it.returnType.classifier == UnicodeEmoji::class }
-//            .forEach { jdaProperty ->
-//                val unicodeEmoji = jdaProperty.getAccessible() as UnicodeEmoji
-//                val emoji = jemojiProperties.first { it.name == jdaProperty.name }.getAccessible() as Emoji
-//
-//                assertEquals(emoji.emoji, unicodeEmoji.name)
-//            }
+        UnicodeEmojis::class.staticProperties
+            .filter { it.returnType.classifier == UnicodeEmoji::class }
+            .forEach { jdaProperty ->
+                val unicodeEmoji = jdaProperty.getAccessible() as UnicodeEmoji
+                assertTrue(EmojiManager.isEmoji(unicodeEmoji.name))
+            }
     }
 
     private fun <T> KProperty0<T>.getAccessible(): T {
